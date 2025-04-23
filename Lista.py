86b92f94bd74b94251f986b020cf191eaaee7c13
yas-vic <yas.vic@hotmail.com>
2025-04-23 20:28:26 -0300

    Add files via upload

---
+++ Lista.py
@@ -0,0 +1,9 @@
+# Crie uma lista com 10 numeros e mostre a média dos numeros na tela
+somas=0
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+soma = sum(lista)
+quantidade = len(lista)
+
+
+print(soma/quantidade)
---
+++ lista 2.py
@@ -0,0 +1,7 @@
+# Crie uma lista com numeros de 1 a 10  mostre apenas os pares utilizando o for
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+for i in lista:
+    if i % 2 ==0:
+        print(i)
